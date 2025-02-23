672abc2524884100bff9cc5ad8b0d000d86643df
JustCromo <ivanurlini@gmail.com>
2025-02-23 11:03:52 +0100

    grr

--- Universita'/JavaScript/sommaSotto.js
+++ Universita'/JavaScript/sommaSotto.js
@@ -16,4 +16,9 @@ function sommaSotto(T){
 dato un albero binario avente come attributi i valori val, sx, dx e res, scrivere una funzione sommaSotto(T)
 che scriva nell'attributo res di ogni nodo t la somma dei valori (val) di tutti i nodi appartenenti al sotto-albero 
 di t (considerando anche t nella somma)
+
+
+Determinare la complessita' della funzione appena scritta --> O(n)
+
  */
+
