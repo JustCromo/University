693f7f8c9c0555a1c1d03ac736b1c25663c58956
JustCromo <ivanurlini@gmail.com>
2025-03-18 18:17:18 +0100

    gg

--- Universita'/TypeScript/Vec.ts
+++ Universita'/TypeScript/Vec.ts
@@ -1,3 +1,43 @@
+type Vettore = {
+    x:number;
+    y:number;
+}
+
 class Vec{
+
+    vet:Vettore;
+
+    constructor(x:number, y:number){
+        this.vet.x = x;
+        this.vet.y = y;
+    }
+
+    sum(w:Vettore):Vettore{
+        
+        let x = this.vet.x + w.x;
+        let y = this.vet.y + w.y;
+        let k:Vettore = {x, y}
+
+        return k;
+    }
+
+    mul(a:Vettore):Vettore{
+        
+        let x = this.vet.x * a.x;
+        let y = this.vet.y * a.y;
+        let k:Vettore = {x, y}
+
+        return k;
+    }
+
+    eq(w:Vettore):boolean{
+
+        if(this.vet.x == w.x && this.vet.y == w.y) return true;
+
+        return false;
+    }
+
     
+
+
 }
