bb2cd7424e4f2c2a71e727670e009667cfc7b795
JustCromo <ivanurlini@gmail.com>
2025-04-01 11:00:08 +0200

    BTSKoreaOfficial

--- Universita'/TypeScript/DFS.ts
+++ Universita'/TypeScript/DFS.ts
@@ -1,3 +1,56 @@
 interface Nodo{
-    
+    color:string,
+    p:Nodo|null,
+    d:number,
+    f:number,
+    val:number,
+    adj:Nodo[]
+};
+
+interface Graph{
+    V:Nodo[],
+    E:[Nodo, Nodo][]
+}
+
+let time;
+
+function DFS(G:Graph):void{
+
+    for(let u of G.V){
+        u.color = "BIANCO";
+        u.p = null;
+    }
+
+    time = 0;
+
+    for(let u of G.V){
+        if(u.color == "BIANCO"){
+            DFS_Visit(u);
+        }
+    }
+
+    return;
+
+}
+
+
+function DFS_Visit(u:Nodo):void{
+
+    u.color = "GRIGIO";
+    time = time+1;
+    u.d = time;
+
+    for(let v of u.adj){
+        if(v.color == "BIANCO"){
+            v.p = u;
+            DFS_Visit(v);
+        }
+    }
+
+    u.color = "NERO";
+    time = time+1;
+    u.f = time;
+
+    return;
+
 }
